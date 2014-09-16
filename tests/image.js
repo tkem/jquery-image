66785eea8101c80242427d1317281a3d33f92de0
Thomas Kemmer <tkemmer@computer.org>
2014-09-16 06:41:24 +0200

    Remove unused .

--- tests/image.js
+++ tests/image.js
@@ -4,8 +4,7 @@
     QUnit.module("jQuery.image");
 
     QUnit.asyncTest("load non-existent image", 1, function(assert) {
-        var $fixture = $("#qunit-fixture"),
-            image = $.image("notfound.jpg");
+        var image = $.image("notfound.jpg");
         image.fail(function() {
             assert.ok(true);
             QUnit.start();
